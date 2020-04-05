fa1fd9da2fed241d2ac9b3ebfc20a718d73b1765
Kynel <47177212+Kynel@users.noreply.github.com>
2020-04-05 22:54:21 +0900

    #7 semantic-ui-css 추가, 오류 수정
    #7 semantic-ui-css 추가, 오류 수정

---
+++ bubblit/assets/js/Board.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Hello extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        );
+    }
+}
+
+export default Hello
