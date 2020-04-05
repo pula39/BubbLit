4a0fed409801cd57671b348918b34186dc0c264b
Kynel <47177212+Kynel@users.noreply.github.com>
2020-04-05 23:19:45 +0900

    #7 semantic-ui-react 설치, 테스트
    #7 semantic-ui-react 설치, 테스트

--- bubblit/assets/js/Board.js
+++ bubblit/assets/js/Board.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { Button } from 'semantic-ui-react'
 class Hello extends React.Component {
 
     render() {
         return (
             <div>
                 <h1>Hello React!</h1>
+                <Button>Test</Button>
             </div>
         );
     }
