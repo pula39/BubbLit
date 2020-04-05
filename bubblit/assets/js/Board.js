69cb3bfa524e774dc6d7125dd8f425607710ef64
Kynel <47177212+Kynel@users.noreply.github.com>
2020-04-06 01:58:17 +0900

    #7 Grid 테스트
    #7 Grid 테스트

--- bubblit/assets/js/Board.js
+++ bubblit/assets/js/Board.js
@@ -1,12 +1,48 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Grid, Divider } from 'semantic-ui-react'
 class Board extends React.Component {
 
     render() {
         return (
             <div>
-                <h1>Hello React!</h1>
+                <h1>BubbLIT Prototype😊</h1>
                 <Button>Test</Button>
+                <Grid columns={2} divided={true}>
+                    <Grid.Column>
+                        <p>
+                            asdfasdfasdf
+                        </p>
+                        <p>
+                            asdfasdfasdf
+                        </p>
+                        <p>
+                            asdfasdfasdf
+                        </p>
+                        <p>
+                            asdfasdfasdf
+                        </p>
+                        <p>
+                            asdfasdfasdf
+                        </p>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <p>
+                            dfasfdasfd
+                        </p>
+                        <p>
+                            dfasfdasfd
+                        </p>
+                        <p>
+                            dfasfdasfd
+                        </p>
+                        <p>
+                            dfasfdasfd
+                        </p>
+                        <p>
+                            dfasfdasfd
+                        </p>
+                    </Grid.Column>
+                </Grid>
             </div>
         );
     }
