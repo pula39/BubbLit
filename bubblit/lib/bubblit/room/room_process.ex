efd1a0b9b080cc926308e9b466f25f32454cb3b2
pula39 <programmed_life@naver.com>
2020-04-20 04:54:06 +0900

    #23 Room Process도 Via Tuple을 통해 Registry를 거치게 해 이름으로 가져올 수 있게 한다.

--- bubblit/lib/bubblit/room/room_process.ex
+++ bubblit/lib/bubblit/room/room_process.ex
@@ -4,7 +4,13 @@ defmodule Bubblit.Room.Process do
   require Util
 
   def start_link(init_arg) do
-    Supervisor.start_link(__MODULE__, init_arg)
+    Util.log("#{inspect(init_arg)} 가지고 #{__MODULE__} 을 시작")
+    [room_id: room_id] = init_arg
+    Supervisor.start_link(__MODULE__, init_arg, name: via_tuple(room_id))
+  end
+
+  defp via_tuple(room_id) do
+    {:via, Bubblit.Room.Registry, {:room_process, room_id}}
   end
 
   def init(init_arg) do
