2575e30efec40ddcd765cb2e234d7ac0287ab175
RE-A <skyseven73@gmail.com>
2020-04-23 03:59:20 +0900

    이미지 영역 사진 추가

--- bubblit/assets/js/Component/ShareSpace.js
+++ bubblit/assets/js/Component/ShareSpace.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import { Tab } from 'semantic-ui-react'
 import Cat from '../../static/images/cat.jpg'
-import Youtubepanel from './ShareSpaceComponent/youtube'
-import Docspanel from './ShareSpaceComponent/googledocs'
+import YoutubePanel from './ShareSpaceComponent/youtube'
+import DocsPanel from './ShareSpaceComponent/googledocs'
+import ImagePanel from './ShareSpaceComponent/shareimage'
 import './../../css/shareSpace.css'
 
 export default class ShareSpace extends Component {
     render() {
         const panes = [
-            { menuItem: 'Youtube', render: () => <Tab.Pane className="sharespace-tab"><Youtubepanel /></Tab.Pane> },
-            { menuItem: 'Docs', render: () => <Tab.Pane className="sharespace-tab"><Docspanel /></Tab.Pane> },
+            { menuItem: 'Youtube', render: () => <Tab.Pane className="sharespace-tab"><YoutubePanel /></Tab.Pane> },
+            { menuItem: 'Docs', render: () => <Tab.Pane className="sharespace-tab"><DocsPanel /></Tab.Pane> },
             { menuItem: 'Log', render: () => <Tab.Pane className="sharespace-tab">ChatLog</Tab.Pane> },
-            { menuItem: 'IMG', render: () => <Tab.Pane className="sharespace-tab"><img src={Cat} alt="이미지"></img></Tab.Pane> },
+            { menuItem: 'IMG', render: () => <Tab.Pane className="sharespace-tab"><ImagePanel /></Tab.Pane> },
         ]
 
         return (
