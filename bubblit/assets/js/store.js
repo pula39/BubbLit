13410718a8e6c4ff270c5c46f932ef449cd2cb8a
Kynel <47177212+Kynel@users.noreply.github.com>
2020-04-26 07:01:34 +0900

    #35 store.js의 state, dispatch 정리
    store.js에서 channel 구독, 해지 구현  및 state 정리

--- bubblit/assets/js/store.js
+++ bubblit/assets/js/store.js
@@ -5,6 +5,7 @@ export default createStore(function (state, action) {
         state = {
             // 초기화할때 소켓을 생성만 해둠, 밑에 ENTER_CHAT 이벤트때 전달받은 세부 정보로 소켓을 connect함
             socket: new Socket('/socket', { params: { token: window.userToken } }),
+            // 채널은 아래의 'ENTER_CHAT' dispatch때 생성해줌
             channel: '',
             mode: 'lobby',
             userName: 'kynel',
@@ -17,34 +18,22 @@ export default createStore(function (state, action) {
                     { id: 4, title: '5st Room', host: 'REA', isPrivate: 'X', limit: 3, current: 1 },
                 ],
             },
-            chatRoom: {
-                chat: '',
-                id: '',
-                time: '',
-                showID: '',
-            },
+            //ChatModule
+            contents: [[], [], [], [], [], []],
+            participants: [],
         }
         return state;
     }
     if (action.type === 'ENTER_CHAT') {
-        //console.log(state.socket === '');
-        //if (state.socket === '') {
-        //    let newSocket = new Socket('/socket', { params: { token: window.userToken } });
-        //    newSocket.connect();
-        //    let newChannel = newSocket.channel('test', { nickname: test });
-        //    newChannel.join()
-        //}
-        //this.setState({ ...state, channel: state.socket.channel('test', { nickname: 'asdf' }) })
-
         // 아래 코드에서 socket을 연결시키고, 방에 들어감과 동시에 channel에 접속시켜준다.
         state.socket.connect();
         return { ...state, mode: action.title, channel: state.socket.channel('room:' + action.title, { nickname: state.userName }) }
     }
     if (action.type === 'CHAT') {
-        return { ...state, chatDB: state.chatDB.concat(action.data) }
+        return { ...state, contents: action.contents, participants: action.participants }
     }
     if (action.type === 'EXIT') {
-        //channel 구독해지
-        return { ...state, mode: 'lobby' }
+        //channel 구독해지 및 state 초기화
+        return { ...state, mode: 'lobby', channel: '', contents: [[], [], [], [], []], participants: [] }
     }
 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
