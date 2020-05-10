d09b769aa1023deb833b657c751fa28055285201
pula39 <programmed_life@naver.com>
2020-05-11 05:15:29 +0900

    #67 main일 경우도 있어서 이것도 redirect

--- bubblit/lib/bubblit_web/router.ex
+++ bubblit/lib/bubblit_web/router.ex
@@ -33,6 +33,7 @@ defmodule BubblitWeb.Router do
     get "/", PageController, :index
 
     # 리액트 싱글 페이지 라우팅 작동 안되서 만든 임시코드
+    get "/main", PageController, :f__k_react
     get "/room", PageController, :f__k_react
     # get "/*path", PageController, :index
   end
