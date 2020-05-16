040ad406bd0af0c4d72a52b24974e31fc209bef0
RE-A <skyseven73@gmail.com>
2020-05-16 19:25:40 +0900

    #84 100% PASS하는 테스트 작성

--- bubblit/config/test.exs
+++ bubblit/config/test.exs
@@ -1,13 +1,5 @@
 use Mix.Config
 
-# Configure your database
-config :bubblit, Bubblit.Repo,
-  username: "root",
-  password: "",
-  database: "bubblit_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :bubblit, BubblitWeb.Endpoint,
@@ -16,3 +8,5 @@ config :bubblit, BubblitWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+import_config "test.secret.exs"
