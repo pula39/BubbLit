9bd0c1b30866b73795e1350bf2e40f948e0706f9
Kynel <47177212+Kynel@users.noreply.github.com>
2020-05-17 18:07:30 +0900

    #0 초기 코드
    #0 초기 코드

--- bubblit/assets/js/Component/ChatModule.js
+++ bubblit/assets/js/Component/ChatModule.js
@@ -7,92 +7,104 @@ import '../../css/chatModule.css'
 import { Link } from 'react-router-dom';
 import { Presence } from "phoenix"
 
+const DEF_CHATBOX_POS = { x: [0, 450, 0, 450, 0, 450], y: [0, 0, 300, 300, 600, 600] }
+const INPUTSPACE_RELATIVE_POS = { x: 0, y: 0.8 };
+
 export default class ChatModule extends Component {
     constructor(props) {
         super(props);
         console.log("chatmodule width, height", window.innerWidth, window.innerHeight)
         this.state = {
             // 방에 나갓다 들어와도 커스텀값이 유지되도록 state로 빼봣음, store로 옮기거나, 수정예정
-            x: [0, 450, 0, 450, 0, 450],
-            y: [0, 0, 300, 300, 600, 600],
+            x: DEF_CHATBOX_POS.x,
+            y: DEF_CHATBOX_POS.y,
             inputMessage: '',
-            chatInputboxX: window.innerWidth * 0,
-            chatInputboxY: window.innerHeight * 0.8,
+            inputspacePosX: window.innerWidth * INPUTSPACE_RELATIVE_POS.x,
+            inputspacePosY: window.innerHeight * INPUTSPACE_RELATIVE_POS.y,
             presences: {}
         }
-        console.log('channelInitializer called');
 
         // 뒤로가기 됐을때를 대비해, 강제로 enterRoom 다시 실행시킴
         // [TODO] 두 과정을 하나로 통합시켜야 함. 하나의 일이 두 과정으로 분리되어 서로 다른 곳에서 실행되고 있어서 이렇게 할 수밖에 없다...
+        // Join을 사용하지 않은 상태에서 방에 입장할 때에 구조적 문제 발생 -> 추후 
         let channelinit = async () => {
             await this.props.enterRoom(this.props.current_room_id)
             await this.channelInitializer();
         }
+
         channelinit()
-        // this.channelInitializer();
+
         this.handleEnterKeyPress = this.handleEnterKeyPress.bind(this)
         this.chatInput = React.createRef()
     }
 
-    channelInitializer() {
-        let joined = this.props.channel.join();
-        // join 할때 처리
-        joined
-            .receive('ok', response => {
-                console.log(response);
-                console.log('joined successfully at ' + response)
-                // bubble_history 받을때 처리
-                this.props.channel.on('room_after_join', payload => {
-                    var changes = { 'participants': payload.room_users, 'contents': { ...this.props.contents } };
-                    console.log('room_after_join', payload);
-                    payload.bubble_history.reverse().forEach(history => {
-                        let user_id = history['user_id'];
-                        let msg = history['content'];
-                        this.addMessageInChanges(changes, user_id, msg);
-                    })
+    onReceiveOk(response) {
+        console.log('joined successfully at ' + response)
+        // bubble_history 받을때 처리
+        this.props.channel.on('room_after_join', payload => {
+            var changes = { 'participants': payload.room_users, 'contents': { ...this.props.contents } };
+            console.log('room_after_join', payload);
 
-                    changes['users'] = payload.users;
-                    this.props.sendChanges(changes);
-
-                    //Users도 같이 들어가있는 room_after_join의 Payload를 그대로 넣고 있음.
-                    //History를 구별해서 넣어야 한다.
-                    //Tab History도 추후에 같이 저장해얌으로 더더욱...
-                    this.props.setHistory(payload);
-                })
-                this.props.channel.on('user_join', payload => {
-                    console.log('user_join', payload);
-                    var changes = { 'participants': this.props.participants, 'contents': { ...this.props.contents }, 'users': { ...this.props.users } };
-                    this.addParticipants(changes, payload.user_id)
-                    changes['users'][payload.user_id] = { id: payload.user_id, name: payload.user_name };
-                    this.props.sendChanges(changes);
-                })
-                this.props.channel.on("new_msg", payload => {
-                    var changes = { 'participants': this.props.participants, 'contents': { ...this.props.contents }, 'users': this.props.users };
-                    let user_id = payload['user_id'];
-                    let msg = payload['body'];
-                    this.addMessageInChanges(changes, user_id, msg);
-                    this.props.sendChanges(changes);
-
-                    //새 메세지를 받을때마다 history state update
-                    let history_payload = { id: this.props.history.bubble_history.length + 1, content: payload.body, user_id: payload.user_id, inserted_at: payload.inserted_at };
-                    let new_history = { ...this.props.history };
-                    new_history.bubble_history = new_history.bubble_history.concat(history_payload);
-                    this.props.appendHistory(new_history);
-                })
-
-                // [TODO] Presence 관련 코드들 Room.js로 옮겨주세요... ShareSpace에서도 필요할듯.
-
-                this.props.channel.on("presence_state", state => {
-                    this.state.presences = Presence.syncState(this.state.presences, state)
-                    console.log("presence_state", this.state.presences)
-                })
-
-                this.props.channel.on("presence_diff", diff => {
-                    this.state.presences = Presence.syncDiff(this.state.presences, diff)
-                    console.log("presence_diff", this.state.presences)
-                })
+            payload.bubble_history.reverse().forEach(history => {
+                let user_id = history['user_id'];
+                let msg = history['content'];
+                this.addMessageInChanges(changes, user_id, msg);
             })
-            .receive('error', response => { console.log('Unable to join', response) })
+
+            changes['users'] = payload.users;
+            this.props.sendChanges(changes);
+
+            //Users도 같이 들어가있는 room_after_join의 Payload를 그대로 넣고 있음.
+            //History를 구별해서 넣어야 한다.
+            //Tab History도 추후에 같이 저장해얌으로 더더욱...
+            this.props.setHistory(payload);
+            this.props.initializeRoomHistory(payload);
+        })
+        this.props.channel.on('user_join', payload => {
+            console.log('user_join', payload);
+            var changes = { 'participants': this.props.participants, 'contents': { ...this.props.contents }, 'users': { ...this.props.users } };
+            this.addParticipants(changes, payload.user_id)
+            changes['users'][payload.user_id] = { id: payload.user_id, name: payload.user_name };
+            this.props.sendChanges(changes);
+            //리팩토링 on
+            this.props.userJoin();
+
+        })
+        this.props.channel.on("new_msg", payload => {
+            var changes = { 'participants': this.props.participants, 'contents': { ...this.props.contents }, 'users': this.props.users };
+            let user_id = payload['user_id'];
+            let msg = payload['body'];
+
+
+            this.addMessageInChanges(changes, user_id, msg);
+            this.props.sendChanges(changes);
+
+            //새 메세지를 받을때마다 history state update
+            let history_payload = { id: this.props.history.bubble_history.length + 1, content: payload.body, user_id: payload.user_id, inserted_at: payload.inserted_at };
+            let new_history = { ...this.props.history };
+            new_history.bubble_history = new_history.bubble_history.concat(history_payload);
+            this.props.appendHistory(new_history);
+        })
+
+        // [TODO] Presence 관련 코드들 Room.js로 옮겨주세요... ShareSpace에서도 필요할듯.
+
+        this.props.channel.on("presence_state", state => {
+            this.state.presences = Presence.syncState(this.state.presences, state)
+            console.log("presence_state", this.state.presences)
+        })
+
+        this.props.channel.on("presence_diff", diff => {
+            this.state.presences = Presence.syncDiff(this.state.presences, diff)
+            console.log("presence_diff", this.state.presences)
+        })
+    }
+
+
+    channelInitializer() {
+        this.props.channel.join()
+            .receive('ok', this.onReceiveOk.bind(this))
+            .receive('error', response => { console.log('Unable to join', response) });
+
         // new_msg 받을때 처리
         //this.props.channel.push('new_msg', { body: '테스트 메세지임니담', nickname: 'tester' });
     }
@@ -110,6 +122,7 @@ export default class ChatModule extends Component {
     }
 
     // chatTestModule 함수 가져와서 사용
+    // stor.js에 옮김
     addMessageInChanges(changes, user_id, msg) {
         let new_member_join_change = msg == null
         if (new_member_join_change) {
@@ -123,11 +136,6 @@ export default class ChatModule extends Component {
         if (msg == null) {
             return;
         }
-        // if (modified_contents[user_idx].length >= 5) {
-        //     //스크롤바 테스트를 위해 잠시 지워둠
-        //     //modified_contents[user_idx].shift();
-        // }
-
         let user_idx = changes.participants.findIndex(find_user_id)
         let modified_contents = changes.contents;
 
@@ -196,7 +204,7 @@ export default class ChatModule extends Component {
         Enter 키로 상호작용하려면 ref를 추가해야 함. 그런데, Form.Input은 functional한 컴포넌트라 ref를 추가할수 없음.
         따라서, Form.Input을 Form과 input으로 분리함.
         @참고 : https://stackoverflow.com/questions/53420516/react-ref-binding-results-in-typeerror-cannot-read-property-focus-of-null
-
+     
         채팅 입력창 위치의 경우, 해당 div 안에서만 최초 위치 설정이 가능함. 따라서 div 설정을 먼저 해야 제대로 옮길 수 있을 거고, 현재는 구석에 뒀음.
         */
         return (
@@ -206,9 +214,9 @@ export default class ChatModule extends Component {
                     <Rnd enableResizing="False"
                         size={{ width: '400', height: '30' }}
                         bounds='window'
-                        position={{ x: this.state.chatInputboxX, y: this.state.chatInputboxY }}
+                        position={{ x: this.state.inputspacePosX, y: this.state.inputspacePosY }}
                         onDragStop={(e, d) => {
-                            this.setState({ chatInputboxX: d.x, chatInputboxY: d.y });
+                            this.setState({ inputspacePosX: d.x, inputspacePosY: d.y });
                         }}>
                         <Form onSubmit={function (e, data) { this.sendChat() }.bind(this)}>
                             <Form.Group>
