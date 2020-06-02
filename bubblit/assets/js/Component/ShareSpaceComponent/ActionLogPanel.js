d7d9eadcdc522d21de4b7ee2233ebcecae325803
Kynel <47177212+Kynel@users.noreply.github.com>
2020-06-02 16:29:11 +0900

    #106 기능 히스토리에 액션 종류 필터기능 추가

--- bubblit/assets/js/Component/ShareSpaceComponent/ActionLogPanel.js
+++ bubblit/assets/js/Component/ShareSpaceComponent/ActionLogPanel.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
-import { Button, Comment, Form, Header } from 'semantic-ui-react'
+import { Button, Comment, Form, Header, Dropdown, Grid } from 'semantic-ui-react'
 import { Scrollbars, scrollToBottom } from 'react-custom-scrollbars';
 
 export default class ActionLogPanel extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showByUser: 'all',
+            showByType: 'all',
+            typeOption: [
+                { text: 'all', value: 'all' },
+                { text: 'control', value: 'control' },
+                { text: 'media', value: 'media' },
+                { text: 'image', value: 'img' },
+            ]
+        }
+
         this.scrollbarRef = React.createRef();
     }
 
@@ -24,30 +35,55 @@ export default class ActionLogPanel extends Component {
         this.handleUpdate();
     }
 
+
     historyRenderer() {
         let contents = [];
+        let showtype = this.state.showByType
         if (this.props.history == undefined ||
             this.props.roomInfo.users === undefined
         ) {
             return [];
         }
+
         this.props.history.forEach(function (item, index, array) {
+            if ((item.type.indexOf(showtype) == -1) && (showtype != 'all'))
+                return true
+
             contents.push(
                 <div key={index}>
                     <p>{item.user_id} {item.type} {item.param}</p>
                 </div>
             );
         })
-        return contents;
+        return contents.reverse();
     }
+
+    handleChange = (e, { name, value }) => {
+        this.setState({ [name]: value })
+        console.log(this.state);
+    }
+
 
     render() {
         return (
             < div >
+                <Form>
+                    <Form.Field>
+                        <label>Select room action type</label>
+                        <Dropdown
+                            selection
+                            name='showByType'
+                            placeholder='Choose action type'
+                            options={this.state.typeOption}
+                            onChange={this.handleChange}
+                        >
+                        </Dropdown>
+                    </Form.Field>
+                </Form>
                 <Scrollbars
                     autoHide={true}
                     ref={this.scrollbarRef}
-                    style={{ height: window.innerHeight * 0.8 }}
+                    style={{ height: window.innerHeight * 0.7 }}
                 >
                     <Comment.Group>
                         {this.historyRenderer()}
